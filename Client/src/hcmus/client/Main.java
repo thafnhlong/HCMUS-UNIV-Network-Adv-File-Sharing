638c03d5e17f2733f2e535402d20187aabc668b8
DangKhoa1999 <buidangkhoa357@gmail.com>
2021-05-09 16:06:13 +0700

    create UI connection master file

--- Client/src/hcmus/client/Main.java
+++ Client/src/hcmus/client/Main.java
@@ -1,8 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package hcmus.client;
 
+import hcmus.client.gui.ConnectionForm;
+
+/**
+ *
+ * @author Admin
+ */
 public class Main {
 
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        System.out.println("This is Client");
+        // TODO code application logic here
+        ConnectionForm connectionForm = new ConnectionForm();
+        connectionForm.setVisible(true);
     }
+    
 }
