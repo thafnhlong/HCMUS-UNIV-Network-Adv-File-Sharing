400aacc7923b5ef0707b916e1343f14bde4b3482
DangKhoa1999 <buidangkhoa357@gmail.com>
2021-05-16 20:34:11 +0700

    create UI list server and handler file

--- Client/src/hcmus/client/Main.java
+++ Client/src/hcmus/client/Main.java
@@ -5,7 +5,7 @@
  */
 package hcmus.client;
 
-import hcmus.client.gui.ConnectionForm;
+import hcmus.client.gui.ConnetionForm;
 
 /**
  *
@@ -18,7 +18,7 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        ConnectionForm connectionForm = new ConnectionForm();
+        ConnetionForm connectionForm = new ConnetionForm();
         connectionForm.setVisible(true);
     }
     
